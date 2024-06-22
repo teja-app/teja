e0d324c25fc26c4aa1f77cbd3f9255cac661c8c6
Aman Kumar <aman@dentira.com>
2024-06-22 17:45:54 +0530

    TA-131 scroll fix

--- lib/presentation/profile/page/profile_main_body.dart
+++ lib/presentation/profile/page/profile_main_body.dart
@@ -1,29 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 import 'package:teja/domain/redux/app_state.dart';
 import 'package:teja/domain/redux/profile_page_sequence/profile_page_actions.dart';
 import 'package:teja/presentation/profile/ui/profile_heat_map.dart';
 import 'package:teja/presentation/profile/ui/profile_mood_activity_screen.dart';
 import 'package:teja/presentation/profile/ui/profile_mood_sleep_chart.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter/material.dart';
 import 'package:teja/presentation/profile/ui/profile_mood_yearly_heatmap.dart';
 
 class MainBody extends StatelessWidget {
-  const MainBody({Key? key});
+  const MainBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, _ViewModel>(
       converter: (store) => _ViewModel.fromStore(store),
-      onInit: (store) => store.dispatch(FetchChartSequenceAction()),
       builder: (context, vm) => vm.isLoading
           ? const Center(child: CircularProgressIndicator())
           : ReorderableListView(
-              reverse: true,
-              scrollController: ScrollController(initialScrollOffset: 10),
-              onReorder: (oldIndex, newIndex) {
-                vm.updateChartSequence(oldIndex, newIndex);
-              },
+              scrollController: ScrollController(),
+              onReorder: vm.updateChartSequence,
               children: vm.chartSequence
                   .map((chart) => _buildChart(context, chart))
                   .toList(),
@@ -67,7 +63,7 @@ class _ViewModel {
     return _ViewModel(
       isLoading: store.state.profilePageState.isLoading,
       chartSequence: store.state.profilePageState.chartSequence,
-      updateChartSequence: (int oldIndex, int newIndex) {
+      updateChartSequence: (oldIndex, newIndex) {
         final updatedSequence =
             List<String>.from(store.state.profilePageState.chartSequence);
         if (oldIndex < newIndex) {
