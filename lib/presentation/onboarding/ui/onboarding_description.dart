ab955496d198e46675e43a8d5eeb6ee25366cada
Aman <amanhuriya3@gmail.com>
2024-08-06 22:38:49 +0530

    TA-234

--- lib/presentation/onboarding/ui/onboarding_description.dart
+++ lib/presentation/onboarding/ui/onboarding_description.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teja/presentation/navigation/isDesktop.dart';
 
 class OnboardingDescription extends StatelessWidget {
   const OnboardingDescription({Key? key}) : super(key: key);
@@ -6,12 +7,14 @@ class OnboardingDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: Text(
-        "Discover your journey towards a balanced life with personalized journaling, mood tracking, and self-improvement strategies.",
-        style: Theme.of(context).textTheme.bodyMedium,
-        textAlign: TextAlign.center,
-      ),
-    );
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: SizedBox(
+          width: isDesktop(context) ? 500 : double.infinity,
+          child: Text(
+            "Discover your journey towards a balanced life with personalized journaling, mood tracking, and self-improvement strategies.",
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ));
   }
 }
