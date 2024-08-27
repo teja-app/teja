a4d860b06ff1d121f09d903d68c0ad7f2b9dd3a5
Aman kumar Huriya <106256434+AmanKumar-0@users.noreply.github.com>
2024-08-27 14:51:52 +0530

    TA-278 Enable notification (#93)

--- lib/infrastructure/utils/notification_service.dart
+++ lib/infrastructure/utils/notification_service.dart
@@ -6,22 +6,25 @@ import 'package:flutter_timezone/flutter_timezone.dart';
 import 'dart:io' show Platform;
 
 class NotificationService {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-  /// Initializes the notification service
   Future<void> initialize() async {
     await _configureTimeZone();
 
-    final DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings(
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
       requestSoundPermission: false,
       requestBadgePermission: false,
       requestAlertPermission: false,
       onDidReceiveLocalNotification: onDidReceiveLocalNotification,
     );
 
-    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
 
-    final InitializationSettings initializationSettings = InitializationSettings(
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
       iOS: initializationSettingsIOS,
       android: initializationSettingsAndroid,
     );
@@ -34,19 +37,20 @@ class NotificationService {
     }
   }
 
-  Future onDidReceiveLocalNotification(int? id, String? title, String? body, String? payload) async {}
+  Future onDidReceiveLocalNotification(
+      int? id, String? title, String? body, String? payload) async {}
 
   void selectNotification(NotificationResponse? payload) {
     if (payload != null) {
     } else {}
-    // Get.to(() => NotiFiedPage(label: '$payload'));
+    // Navigate to the desired page
   }
 
-  /// Requests permission for notifications on iOS
   Future<void> requestIOSPermissions() async {
     if (Platform.isIOS) {
       await flutterLocalNotificationsPlugin
-          .resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
           ?.requestPermissions(
             alert: true,
             badge: true,
@@ -60,62 +64,108 @@ class NotificationService {
         await flutterLocalNotificationsPlugin.pendingNotificationRequests();
 
     int newId = 100; // Starting base value
-    while (pendingNotifications.any((notification) => notification.id == newId)) {
+    while (
+        pendingNotifications.any((notification) => notification.id == newId)) {
       newId++;
     }
     return newId;
   }
 
   Future<void> scheduleNotification({
+    required int uniqueId,
     required String title,
     required String body,
     required int hour,
     required int minute,
+    int daysAhead = 30, // Number of days to schedule notifications for
     String channelId = 'default_channel_id',
     String channelName = 'Default Channel',
     String channelDescription = 'Default channel description',
     AndroidScheduleMode scheduleMode = AndroidScheduleMode.exact,
+    bool isTestMode = false, // Add a flag for test mode
   }) async {
-    int uniqueId = await getUniqueNotificationId(); // Get a unique ID for the notification
+    for (int i = 0; i < daysAhead; i++) {
+      final int currentNotificationId = uniqueId + i;
 
-    try {
-      await flutterLocalNotificationsPlugin.zonedSchedule(
-        uniqueId, // Use the unique ID
-        title,
-        body,
-        _nextInstanceOfSpecificTime(hour, minute),
-        NotificationDetails(
-          android: AndroidNotificationDetails(channelId, channelName),
-          iOS: const DarwinNotificationDetails(),
-        ),
-        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
-        androidScheduleMode: scheduleMode,
-      );
-    } catch (e) {}
+      try {
+        final tz.TZDateTime scheduledTime = isTestMode
+            ? tz.TZDateTime.now(tz.local).add(Duration(seconds: 10 * i))
+            : _nextInstanceOfSpecificTime(hour, minute, daysAhead: 1);
+
+        await flutterLocalNotificationsPlugin.zonedSchedule(
+          currentNotificationId,
+          // uniqueId,
+          title,
+          body,
+          scheduledTime,
+          // _nextInstanceOfSpecificTime(hour, minute),
+          // tz.TZDateTime.now(tz.local).add(const Duration(seconds: 10 * 1)),
+          NotificationDetails(
+            android: AndroidNotificationDetails(channelId, channelName),
+            iOS: const DarwinNotificationDetails(),
+          ),
+          uiLocalNotificationDateInterpretation:
+              UILocalNotificationDateInterpretation.absoluteTime,
+          androidScheduleMode: scheduleMode,
+        );
+      } catch (e) {
+        // Handle scheduling error
+      }
+    }
   }
 
-  /// Cancels a scheduled notification
-  Future<void> cancelNotification(int id) async {
+  Future<void> cancelAllNotifications() async {
     try {
-      await flutterLocalNotificationsPlugin.cancel(id);
+      await flutterLocalNotificationsPlugin.cancelAll();
     } catch (e) {
       // Handle cancellation failure
     }
   }
 
-  // Configures the timezone settings
+  Future<void> cancelNotification(int id) async {
+    try {
+      for (int i = 0; i < 30; i++) {
+        await flutterLocalNotificationsPlugin.cancel(id + i);
+      }
+    } catch (e) {
+      // Handle cancellation failure
+    }
+  }
+
+  Future<void> refreshDailyNotifications({
+    required int uniqueId,
+    required String title,
+    required String body,
+    required int hour,
+    required int minute,
+  }) async {
+    // Cancel all existing notifications
+    await cancelAllNotifications();
+
+    // Schedule notifications for the next 30 days
+    await scheduleNotification(
+      uniqueId: uniqueId,
+      title: title,
+      body: body,
+      hour: hour,
+      minute: minute,
+      // daysAhead: 30,
+    );
+  }
 
   Future<void> _configureTimeZone() async {
     tz.initializeTimeZones();
-    final String timezone = await FlutterTimezone.getLocalTimezone(); // Get local timezone using flutter_timezone
+    final String timezone = await FlutterTimezone.getLocalTimezone();
     tz.setLocalLocation(tz.getLocation(timezone));
   }
 
-  /// Calculates the next instance of a specific time
-  tz.TZDateTime _nextInstanceOfSpecificTime(int hour, int minute) {
+  tz.TZDateTime _nextInstanceOfSpecificTime(int hour, int minute,
+      {int daysAhead = 0}) {
     final now = tz.TZDateTime.now(tz.local);
-    var scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
-    if (scheduledDate.isBefore(now)) scheduledDate = scheduledDate.add(const Duration(days: 1));
+    var scheduledDate = tz.TZDateTime(
+        tz.local, now.year, now.month, now.day + daysAhead, hour, minute);
+    if (scheduledDate.isBefore(now))
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
     return scheduledDate;
   }
 }
--- lib/infrastructure/utils/time_storage_helper.dart
+++ lib/infrastructure/utils/time_storage_helper.dart
@@ -6,31 +6,41 @@ class TimeStorage {
 
   Future<void> saveTimeSlot(String activity, TimeOfDay timeOfDay) async {
     final box = await Hive.openBox(_boxName);
-    await box.put(activity, timeOfDay.toString());
+    await box.put('${activity}_time', timeOfDay.toString());
+  }
+
+  Future<void> saveEnabledStatus(String activity, bool isEnabled) async {
+    final box = await Hive.openBox(_boxName);
+    await box.put('${activity}_enabled', isEnabled);
   }
 
   Future<Map<String, TimeOfDay>> getTimeSlots() async {
-    try {
-      final box = await Hive.openBox(_boxName);
-      final Map<String, TimeOfDay> timeSlots = {};
-      for (var key in box.keys) {
-        if (key != null && key is String) {
-          final timeString = box.get(key);
-
-          if (timeString != null) {
-            final cleanTimeString =
-                timeString.replaceAll('TimeOfDay(', '').replaceAll(')', '');
-            final timeOfDayParts = cleanTimeString.split(":");
-            final hour = int.parse(timeOfDayParts[0]);
-            final minute = int.parse(timeOfDayParts[1]);
-            timeSlots[key] = TimeOfDay(hour: hour, minute: minute);
-          }
-        }
+    final box = await Hive.openBox(_boxName);
+    final Map<String, TimeOfDay> timeSlots = {};
+    for (var key in box.keys) {
+      if (key != null && key is String && key.endsWith('_time')) {
+        final timeString = box.get(key);
+        final cleanTimeString =
+            timeString.replaceAll('TimeOfDay(', '').replaceAll(')', '');
+        final timeOfDayParts = cleanTimeString.split(":");
+        final hour = int.parse(timeOfDayParts[0]);
+        final minute = int.parse(timeOfDayParts[1]);
+        timeSlots[key.replaceFirst('_time', '')] =
+            TimeOfDay(hour: hour, minute: minute);
       }
-      return timeSlots;
-    } catch (e) {
-      print('Error getting time slots: $e');
-      rethrow;
     }
+    return timeSlots;
+  }
+
+  Future<Map<String, bool>> getEnabledStatuses() async {
+    final box = await Hive.openBox(_boxName);
+    final Map<String, bool> enabledStatuses = {};
+    for (var key in box.keys) {
+      if (key != null && key is String && key.endsWith('_enabled')) {
+        final isEnabled = box.get(key);
+        enabledStatuses[key.replaceFirst('_enabled', '')] = isEnabled ?? false;
+      }
+    }
+    return enabledStatuses;
   }
 }
